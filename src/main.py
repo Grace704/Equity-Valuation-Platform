ff3386868ee0d52d50edd4c5b41fc33e7e4d2184
Grace Qi <qigrace0@gmail.com>
2026-07-10 20:28:03 -0400

    Add more constant into Statistics

--- src/main.py
+++ src/main.py
@@ -1,16 +1,46 @@
 from data_loader import download_price_data, download_financial_statements
+from statistics import (calculate_daily_returns, calculate_average_daily_return, 
+calculate_annalized_return, calculate_annualized_volatility, calculate_sharpe_ratio)
+
 ticker = input("Enter the stock ticker symbol: ").upper()
 try:
     price, file_path = download_price_data(ticker)
+
+    print(f"Saved price data to {file_path}")
     print(price.head())
     print(price.describe())
+
+    daily_returns = calculate_daily_returns(price)
+    print("Daily Returns:")
+    print(daily_returns.head())
+
+    average_daily_return = calculate_average_daily_return(daily_returns)
+    print("Average Daily Return:")
+    print(average_daily_return)
+
+    annualized_return = calculate_annalized_return(daily_returns)
+    print("Annualized Return:")
+    print(annualized_return)
+
+    annualized_volatility = calculate_annualized_volatility(daily_returns)
+    print("Annualized Volatility:")
+    print(annualized_volatility)
+
+    sharpe_ratio = calculate_sharpe_ratio(daily_returns)
+    print("Sharpe Ratio:")
+    print(sharpe_ratio)
+
     income_statement, balance_sheet, cashflow = download_financial_statements(ticker)
+    
     print("Income Statement:")
     print(income_statement.head())
+
     print("\nBalance Sheet:")
     print(balance_sheet.head())
+
     print("\nCash Flow Statement:")
     print(cashflow.head())
-    print(f"Saved price data to {file_path}")
+
 except ValueError as error:
     print(error)
+
---
+++ src/statistics.py
@@ -0,0 +1,32 @@
+def calculate_daily_returns(price_data):
+    daily_returns = price_data["Close"].pct_change()
+    return daily_returns
+
+def calculate_average_daily_return(daily_returns):
+    average_daily_return = daily_returns.mean()
+    return average_daily_return
+
+def calculate_annalized_return(daily_returns, trading_days=252):
+    average_daily_return = calculate_average_daily_return(daily_returns)
+    annualized_return = (1 + average_daily_return) ** trading_days - 1 
+    """Future Value = Present Value × (1 + return)^number_of_periods"""
+    return annualized_return
+
+def calculate_annualized_volatility(daily_returns, trading_days=252):
+    daily_volatility = daily_returns.std()
+    annualized_volatility = daily_volatility * (trading_days ** 0.5)
+    return annualized_volatility
+
+def calculate_sharpe_ratio(daily_returns, risk_free_rate=0.01, trading_days=252):
+    annualized_return = calculate_annalized_return(daily_returns, trading_days)
+    annualized_volatility = calculate_annualized_volatility(daily_returns, trading_days)
+    sharpe_ratio = (annualized_return - risk_free_rate) / annualized_volatility
+    return sharpe_ratio
+
+def calculate_max_drawdown(price_data):
+    daily_returns = price_data["Close"].pct_change().dropna()
+    cumulative_returns = (1 + price_data["Close"].pct_change()).cumprod()
+    peak = cumulative_returns.cummax()
+    drawdown = (cumulative_returns - peak) / peak
+    max_drawdown = drawdown.min()
+    return max_drawdown
